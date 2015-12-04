071a8938f566f5a8046ab689f0bfcaf975aa1a82
Jaily <JailyRancher>
2015-12-03 19:53:17 -0600

    Added tea time message

--- website/hello.py
+++ website/hello.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from flask import redirect, url_for
-import serial
+#import serial
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+#ser = serial.Serial('/dev/ttyACM1', 9600)
 
 app = Flask(__name__)
 message = ""
@@ -17,39 +17,45 @@ def index():
 
 
 # This route will take in an input message and parse the timer from it
-# Message format: B 000 C
+# Message format: B000000
+# The first three numbers are boiling time
+# Second three numbers are tea time 
 @app.route('/send', methods=['POST'])
 def send():
     temp = []
     temp = str(request.form['inputMessage'])
-    strtime = temp[1:-5]
-    time = int(strtime)
-    temperature = request.form['inputMessage']
-    print message
-    ser.write(message)
-    return render_template('index.html', temperature=temperature, time=time)
+    boilingtime = temp[1:-3]
+    teatime = temp[4:]
+    boilingtime = int(boilingtime)
+    message = request.form['inputMessage']
+#    ser.write(message)
+    return render_template('index.html', time=boilingtime)
 
-
+# For testing purposes
 # Test run without the Arduino
 @app.route('/timeset', methods=['POST'])
 def timeset():
     temp = []
     temp = str(request.form['timeset'])
-    strtime = temp[1:-5]
-    time = int(strtime)
-    print type(time)
-    print time
-    return render_template('index.html', time=time, message=message)
+    boilingtime = temp[1:-3]
+    teatime = temp[4:]
+    boilingtime = int(boilingtime)
+    teatime = int(teatime)
+    print boilingtime
+    print teatime
+    return render_template('index.html', time=boilingtime, message=temp)
 
 
 # AJAX Call method
 # Reads in message sent from Arduino and returns it to index.html
 @app.route('/arduino/')
 def readArduino():
-    message=ser.readline()
-    d = {}
-    d['val'] = message
-    return jsonify(**d)
+#    message=ser.readline()
+#    d = {}
+#    d['val'] = message
+#    return jsonify(**d)
+    return "Hello"
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
